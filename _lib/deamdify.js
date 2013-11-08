a3c669397497675942c8954b6f4dc7d79e55e4bb
Andrew Petersen <kirbysayshi@gmail.com>
2013-11-08 02:10:44 -0500

    deamdify: backfill undefined deps

--- _lib/deamdify.js
+++ _lib/deamdify.js
@@ -1,3 +1,26 @@
+/*
+(The MIT License)
+
+Copyright (c) 2013 Jared Hanson
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of
+this software and associated documentation files (the "Software"), to deal in
+the Software without restriction, including without limitation the rights to
+use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of
+the Software, and to permit persons to whom the Software is furnished to do so,
+subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS
+FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR
+COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER
+IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+*/
+
 /**
  * Module dependencies.
  */
@@ -81,6 +104,15 @@ module.exports = function (file) {
             
             var ids = dependencies.elements.map(function(el) { return el.value });
             var vars = factory.params.map(function(el) { return el.name });
+
+            // Backfill in the event that there are not enough defined
+            // callback args (named dependencies) to match dependency
+            // paths:
+            //   define(['a/hey', 'a/what'], function(hey) {});
+            while (vars.length < ids.length) {
+              vars.push('__unnameddep__');
+            }
+
             var reqs = createRequires(ids, vars);
             if (reqs) {
               tast = createProgram([reqs].concat(factory.body.body));
@@ -94,6 +126,15 @@ module.exports = function (file) {
             
             var ids = dependencies.elements.map(function(el) { return el.value });
             var vars = factory.params.map(function(el) { return el.name });
+
+            // Backfill in the event that there are not enough defined
+            // callback args (named dependencies) to match dependency
+            // paths:
+            //   define(['a/hey', 'a/what'], function(hey) {});
+            while (vars.length < ids.length) {
+              vars.push('__unnameddep__');
+            }
+
             var reqs = createRequires(ids, vars);
             if (reqs) {
               tast = createProgram([reqs].concat(factory.body.body));
