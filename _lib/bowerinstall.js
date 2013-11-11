bcd00c15429e997109534b9b8fb9dbbff1ba9cfd
Andrew Petersen <kirbysayshi@gmail.com>
2013-11-11 00:46:04 -0500

    better bower output

--- _lib/bowerinstall.js
+++ _lib/bowerinstall.js
@@ -3,7 +3,9 @@ var bower = require('bower');
 
 module.exports = function(root, installtarget, cb) {
   bower.commands.install([], {}, { cwd: root, directory: installtarget })
-    .on('log', console.log.bind(console))
+    .on('log', function(log) {
+      console.log('bower: [' + log.level + '] ' + log.id + ': ' + log.message);
+    })
     .on('error', cb)
     .on('end', function(results) {
       cb(null, results);
